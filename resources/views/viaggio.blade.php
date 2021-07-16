a99ea654b2be0499b7c60d0841a18d9c9a7a588a
Caterina Bertotti <bertotti4997@gmail.com>
2021-07-16 22:41:34 +0200

    VIAGGIO: intro

--- resources/views/viaggio.blade.php
+++ resources/views/viaggio.blade.php
@@ -28,6 +28,21 @@
 
     @include('layouts/hero/heroBgFlower/heroViaggio')
 
+    <section>
+        <div class="mycontainer container-m">
+            <img class="section__fiore" src="{{asset('storage/img/fiori/papavero.svg')}}" alt="Papavero">
+            <h2 class="section__titolo main__title">la nostra luna di <br class="d-none-xs">“m...armellata”!</h2>
+            <h5>(100% vegan)</h5>
+            <p class="sec__box">Dopo aver viaggiato in lungo in largo e nelle più disparate modalità, ci siamo resi conto di aver dimenticato l’Italia dalla Pianura Padana in giù.</p>
+            <p><strong>ROMA</strong> e <strong>NAPOLI!</strong><p>
+                <p class="mt-10">
+                Ne abbiamo tanto letto e sentito parlare ed ora non vediamo l’ora di farle diventare parte delle nostre avventure. <br class="d-none-xs"> 
+                Il tutto come piace a noi… </p>
+                <p class="mt-10"><strong>ZAINO IN SPALLA, SI PARTE!</strong> </p>
+            <img class="section__cuore" src="{{asset('storage/img/icone/cuore.svg')}}" alt="Icona cuore">
+        </div>
+    </section>
+
     
 
 @endsection
